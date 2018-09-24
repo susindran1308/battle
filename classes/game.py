8bf842795d465182db07a4dd72f396fbf3bb824d
Susindran <susindran.cs16@bitsathy.ac.in>
2018-09-24 06:34:05 +0530

    second commit

--- classes/game.py
+++ classes/game.py
@@ -32,4 +32,37 @@ class Person:
         return random.randrange(mgl, mgh)
 
     def take_damage(self, dmg):
-        self.hp -= dmg   
+        self.hp -= dmg
+
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxmp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
+    def choose_action(self):
+        i = 1
+        print("Actions")
+        for item in self.actions:
+            print(str(i), ":", item)
+            i += 1
+
+    def choose_magic(self):
+        i = 1
+        print("Magic")
+        for spell in self.magic:
+            print(str(i), ":", spell["name"], "cost:", str(spell["cost"]))
+            i += 1
